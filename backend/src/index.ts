11149e20432f7c6887034ff31b161aca9c9669d9
JoojVitor <joaovitordossantoscouto@gmail.com>
2025-11-04 13:14:14 -0300

    corrigido index.ts no backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,34 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { connectDB } from "./db";
-import healthRoutes from "./routes/health";
-import authRoutes from "./routes/auth";
-import profileRoutes from "./routes/profile";
-import courseRoutes from "./routes/courses";
-import enrollmentRoutes from "./routes/enrollments";
-import uploadRoutes from "./routes/upload";
-import adminRoutes from "./routes/admin";
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import { connectDB } from './db'
+import authRoutes from './routes/auth'
+import healthCheckRoutes from './routes/health'
+import courseRoutes from './routes/courses'
+import enrollmentRoutes from './routes/enrollments'
+import profileRoutes from './routes/profile'
+import uploadRoutes from './routes/upload'
+import adminRoutes from './routes/admin'
 
-dotenv.config();
+const app = express()
+const port = process.env.PORT || 3001
 
-const app = express();
-const PORT = Number(process.env.PORT || 4000);
+connectDB()
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://192.168.100.69:3000",
-    ],
-  })
-);
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 
-app.use("/api/profile", profileRoutes);
-app.use("/api/courses", courseRoutes);
-app.use("/api/enrollments", enrollmentRoutes);
-app.use("/api/upload", uploadRoutes);
-app.use("api/admin", adminRoutes);
+app.use(express.json())
 
-app.use("/api/health", healthRoutes);
-app.use("/api/auth", authRoutes);
+app.use('/api/health', healthCheckRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/courses', courseRoutes)
+app.use('/api/enrollments', enrollmentRoutes)
+app.use('/api/profile', profileRoutes)
+app.use('/api/upload', uploadRoutes)
+app.use('/api/admin', adminRoutes)
 
-connectDB().then(() => {
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-  });
-});
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})
